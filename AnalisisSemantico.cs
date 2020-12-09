138bd87fd870a33c7b0274d9be4307b0d4d11ab2
moises97 <moises.rivera@alumnos.udg.mx>
2020-12-08 23:01:07 -0600

    Se hacen pruebas de análisis semántico

--- AnalisisSemantico.cs
+++ AnalisisSemantico.cs
@@ -19,38 +19,11 @@ namespace Compilador
 
         private void declaracionesPrevias(string arbol, List<Token> tablaSimbolos)
         {
-            /*
-            //Validar sumas, restas, multiplicacion, division, and y or
-            Regex suma = new Regex(@"[+]+");
-            if (suma.IsMatch(arbol))
+            Regex variables = new Regex(@"[int|float][a-zA-Z0-9]+;+");
+            if (variables.IsMatch(arbol))
             {
-                MessageBox.Show("Hay suma");
+                MessageBox.Show("Hay variables");
             }
-            Regex resta = new Regex(@"[-]+");
-            if (resta.IsMatch(arbol))
-            {
-                MessageBox.Show("Hay resta");
-            }
-            Regex multi = new Regex(@"[*]+");
-            if (multi.IsMatch(arbol))
-            {
-                MessageBox.Show("Hay multiplicacion");
-            }
-            Regex div = new Regex(@"[/]+");
-            if (div.IsMatch(arbol))
-            {
-                MessageBox.Show("Hay division");
-            }
-            Regex andS = new Regex(@"[&&]+");
-            if (andS.IsMatch(arbol))
-            {
-                MessageBox.Show("Hay and");
-            }
-            Regex orS = new Regex(@"[||]+");
-            if (orS.IsMatch(arbol))
-            {
-                MessageBox.Show("Hay or");
-            }*/
         }
 
         private void asignacionDeValor(string arbol, List<Token> tablaSimbolos)
